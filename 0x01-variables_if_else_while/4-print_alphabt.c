a8d852955fbaea64fb96e6e73be58b50a6edb296
AgboolaAgbeniga <114500552+AgboolaAgbeniga@users.noreply.github.com>
2022-10-17 21:14:15 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-	char lower;
-
-	for (lower = 'a'; lower <= 'z'; lower++)
-
-	{
-
-		if (lower == 'e' || lower == 'q')
-
-			continue;
-
-		else
-
-			putchar (lower);
-
-	}
-
-	putchar ('\n');
-
-	return (0);
-
+char lower;
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+if (lower == 'e' || lower == 'q')
+continue;
+else
+putchar (lower);
+}
+putchar ('\n');
+return (0);
 }
