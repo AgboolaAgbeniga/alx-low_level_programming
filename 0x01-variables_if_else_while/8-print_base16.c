69ddad11f6aa582d362dbbd6050c5c970d35a5f8
AgboolaAgbeniga <114500552+AgboolaAgbeniga@users.noreply.github.com>
2022-10-17 21:16:13 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,41 +1,21 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-	int i;
-
-	char lower;
-
-	for (i = 0; i <= 9; i++)
-
-	{
-
-		putchar((i % 10) + '0');
-
-	}
-
-	for (lower = 'a'; lower <= 'f'; lower++)
-
-	{
-
-		putchar (lower);
-
-	}
-
-	putchar ('\n');
-
-	return (0);
-
+int i;
+char lower;
+for (i = 0; i <= 9; i++)
+{
+putchar((i % 10) + '0');
+}
+for (lower = 'a'; lower <= 'f'; lower++)
+{
+putchar (lower);
+}
+putchar ('\n');
+return (0);
 }
