e76b393c0e1f31beaf0b447daa26fa1e6f8226a1
AgboolaAgbeniga <114500552+AgboolaAgbeniga@users.noreply.github.com>
2022-10-17 21:27:15 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,26 +6,36 @@
  */
 int main(void)
 {
-int number1, number2;
-for (number1 = 0; number1 < 10; number1++)
-{
-for (number2 = 0; number2 < 10; number2++)
-{
-if (number1 == number2 || number1 > number2)
-continue;
-else
-putchar ((number1 % 10) + '0');
-putchar ((number2 % 10) + '0');
-if (number1 == 8 && number2 == 9)
-{
-}
-else
-{
-putchar (',');
-putchar (' ');
-}
-}
-}
-putchar('\n');
-return (0);
+
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+			for (k = j; k < 58; k++)
+			{
+				if (i == j || j == k || i == k)
+				{
+					continue;
+				}
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i == 55 && j == 56 && k == 57)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
