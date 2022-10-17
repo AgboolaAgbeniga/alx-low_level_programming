5fdf6fac8ebedd28da4e89ee13393f98486a8355
AgboolaAgbeniga <114500552+AgboolaAgbeniga@users.noreply.github.com>
2022-10-17 21:14:58 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-	int numb;
-
-	for (numb = 0; numb < 10; numb++)
-
-	{
-
-		putchar ((numb % 10) + '0');
-
-	}
-
-	putchar('\n');
-
-	return (0);
-
+int numb;
+for (numb = 0; numb < 10; numb++)
+{
+putchar ((numb % 10) + '0');
+}
+putchar('\n');
+return (0);
 }
