c839e7eb8d65ca3caae7da9c58c817dc11b6d87e
AgboolaAgbeniga <114500552+AgboolaAgbeniga@users.noreply.github.com>
2022-10-17 21:16:41 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,41 +1,21 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-	int i;
-
-	for (i = 0; i < 10; i++)
-
-	{
-
-		putchar((i % 10) + '0');
-
-		if (i == 9)
-
-			continue;
-
-		else
-
-			putchar (',');
-
-		putchar (' ');
-
-	}
-
-	putchar ('\n');
-
-	return (0);
-
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar((i % 10) + '0');
+if (i == 9)
+continue;
+else
+putchar (',');
+putchar (' ');
+}
+putchar ('\n');
+return (0);
 }
