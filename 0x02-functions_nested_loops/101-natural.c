59229c44e4fed3d3a021336fdab2bd8dfdfde66d
AgboolaAgbeniga <agboolaagbeniga@gmail.com>
2022-10-19 14:53:11 -0700

    corrected 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
-#include<conio.h>
-
 /**
- * print_times_table - prints times table
- *@n: is intiger variable for print times table
- *
- * Return: Always 0.
- */
-int main()
+*main - prints count of multiples
+*of 3 or 5 below 1024
+*Return: return 0
+*/
+int main(void)
 {
-    long unsigned int i,sum=0;
-    clrscr();
-    for(i=0;i<1000;i++)
-    {
-        if((i%5==0)||(i%3==0))
-        {
-            sum=sum+i;
-        }
-    }
-    printf("%ld\n",sum);
-    getchar();
-    return 0;
+int n, sum = 0;
+for (n = 0; n < 1024; n++)
+{
+if ((n % 3) == 0 || (n % 5) == 0)
+sum += n;
+}
+printf("%d\n", sum);
+return (0);
 }
