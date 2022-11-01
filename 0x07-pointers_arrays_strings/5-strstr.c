457b0224fbd85ef5f05acf3f2290697a7039ebda
AgboolaAgbeniga <agboolaagbeniga@gmail.com>
2022-11-01 15:34:21 -0700

    corrected dome inputs

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,24 +1,33 @@
 #include "main.h"
 
 /**
- * _strstr - funtion locates a substring.
+ * _strstr - locates a substring
  *
- * @haystack:  This is the main C string to be scanned.
- * @needle: This is the small string to be searched with-in haystack string.
+ * @haystack: the longer string to search
+ * @needle: the first occurrence of the substring
  *
- * Return: Returns the transformed pointer.
- *
-**/
+ * Return: a pointer beg of substring or @Null if it not foound.
+ */
 char *_strstr(char *haystack, char *needle)
 {
-int i;
+	unsigned int i = 0, j = 0;
 
-for (i = 0; haystack[i]; i++)
-if (haystack[i] == needle[0])
-return (haystack + i);
-
-if (haystack[i] == needle[0])
-return (haystack + i);
-
-return (0);
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
 }
