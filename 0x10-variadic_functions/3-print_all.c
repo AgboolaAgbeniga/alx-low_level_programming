7f9c5fbec1040aa9c136f8420013c3ab57e00da5
AgboolaAgbeniga <agboolaagbeniga@gmail.com>
2022-11-17 15:26:03 -0800

    Task 3 corrected

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,54 +1,51 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything
- * @format: any variable type
- * Return: Nothing
-**/
+ * print_all - Prints all of the arguments when specified
+ * @format: specifies the necessary operations
+ * Return: void
+ */
 
 void print_all(const char * const format, ...)
 {
-unsigned int i = 0, j = 0;
-char *str;
-va_list ls;
-va_start(ls, format);
+	int i;
+	int flag;
+	char *str;
+	va_list a_list;
 
-while (format && format[i])
-{
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(ls, int));
-break;
-case 'i':
-printf("%i", va_arg(ls, int));
-break;
-case 'f':
-printf("%f", va_arg(ls, double));
-break;
-case 's':
-str = va_arg(ls, char *);
-if (str == NULL)
-str = "(nil)";
-
-printf("%s", str);
-break;
-}
-j = i + 1;
-
-while (format[j] && (format[j] == 'c' || format[j] == 'i' ||
-format[j] == 'f' || format[j] == 's'))
-{
-printf(", ");
-break;
-}
-
-i++;
-}
-printf("\n");
-va_end(ls);
-return;
+	va_start(a_list, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && flag == 0)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(a_list);
 }
